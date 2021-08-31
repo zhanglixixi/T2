79cb61613f07b10ff9d499d57572dc4ad6409592
zhangliyu <39975881+zhanglixixi@users.noreply.github.com>
2021-08-31 11:46:51 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.append('../../')
+sys.path.append('.')
 # sys.path.append('../../transcript_select')
 
 sys.path.insert(0, os.path.abspath('../../transcript_select'))
